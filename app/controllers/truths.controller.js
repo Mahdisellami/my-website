e933c856afb6ee8b32e2209922b87664242cf357
Mahdisellami <mahdi.sellami.95@gmail.com>
2023-11-14 22:02:11 +0100

    first commit
    Setup ExpressJS

---
+++ app/controllers/truths.controller.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+const Truth = db.truths;
+
+// Create and Save a new Truth
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all Truths from the database.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single Truth with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Truth by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Truth with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all Truths from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all published Truths
+exports.findAllPublished = (req, res) => {
+  
+};

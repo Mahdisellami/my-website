8d18742690960ea60eae72f38bf43fa8ea503a2e
Mahdisellami <mahdi.sellami.95@gmail.com>
2023-11-19 16:51:23 +0100

    finished login and token verification in backend

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -2,8 +2,12 @@ module.exports = app => {
   const auth = require("../controllers/auth.controller.js");
   var router = require("express").Router();
 
+  // Verify user
+  router.post('/', auth.verify)
   // Signup a new user
   router.post("/signup", auth.signup);
+  // Login user
+  router.post('/login', auth.login)
 
   app.use('/api/auth', router);
 };
--- backend/app/util/secret.token.js
+++ backend/app/util/secret.token.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
-
 const jwt = require("jsonwebtoken");
-console.log("TOKEN")
-console.log(process.env.TOKEN_KEY);
 
 module.exports.createSecretToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_KEY, {

f263ec3de8dd82281d177bcb21793b21a88e0317
Mahdisellami <mahdi.sellami.95@gmail.com>
2023-11-19 17:05:39 +0100

    Added login and home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 import AddTruth from "./components/AddTruth";
 import Truth from "./components/Truth";
 import TruthsList from "./components/TruthsList";
@@ -11,10 +15,20 @@ function App() {
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/truths" className="navbar-brand">
+        <a href="/" className="navbar-brand">
           bahta
         </a>
         <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/login"} className="nav-link">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/signup"} className="nav-link">
+              Sign Up
+            </Link>
+          </li>
           <li className="nav-item">
             <Link to={"/truths"} className="nav-link">
               Truths
@@ -30,7 +44,9 @@ function App() {
 
       <div className="container mt-3">
         <Routes>
-          <Route path="/" element={<AddTruth/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/truths" element={<TruthsList/>} />
           <Route path="/add" element={<AddTruth/>} />
           <Route path="/truths/:id" element={<Truth/>} />
